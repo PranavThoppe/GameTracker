f482cde750cc8af2c7e4a47f40e03d1366395660
pthoppe <prv.pats@gmail.com>
2025-09-02 21:53:22 -0500

    Ready for db change

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,17 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "prefer-const": "warn",
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
